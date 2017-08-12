f315bcea0f44a172cd13753411ca70b28325b63d
QUANGMINH\QuangMinh <nguyenquangminh2912@gmail.com>
2017-08-12 16:09:33 +0700

    Layout Trang admin, Webgrid

---
+++ WebsiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class QuanLySanPhamController : Controller
+    {
+        // GET: QuanLySanPham
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        public ActionResult Index()
+        {
+            return View(db.SanPhams.Where(n=>n.DaXoa==false));
+        }
+    }
+}
