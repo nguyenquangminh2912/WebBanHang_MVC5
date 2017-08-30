37e33fcac2c1b37c12fd9aa0d3f278c219c425d2
QuangMinh <nguyenquangminh2912@gmail.com>
2017-08-30 16:29:27 +0700

    Tao moi san pham

--- WebsiteBanHang/Controllers/QuanLySanPhamController.cs
+++ WebsiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using WebsiteBanHang.Models;
 
@@ -11,6 +12,22 @@ namespace WebsiteBanHang.Controllers
         public ActionResult Index()
         {
             return View(db.SanPhams.Where(n=>n.DaXoa==false));
+        }
+
+        [HttpGet]
+        public ActionResult TaoMoi()
+        {
+            //Xư lý dropdownlist
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n =>n.TenNCC), "MaNCC", "TenNCC");
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai");
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TaoMoi(HttpPostedFileBase HinhAnh)
+        {
+            return View();
         }
     }
 }
