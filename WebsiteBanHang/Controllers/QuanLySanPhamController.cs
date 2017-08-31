b72549a632c4cf09c7f4660498449c13be674011
QuangMinh <nguyenquangminh2912@gmail.com>
2017-08-31 16:13:52 +0700

    Tao moi san pham(tt)31/08/2017

--- WebsiteBanHang/Controllers/QuanLySanPhamController.cs
+++ WebsiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebsiteBanHang.Models;
@@ -11,7 +12,7 @@ namespace WebsiteBanHang.Controllers
         QuanLyBanHangEntities db = new QuanLyBanHangEntities();
         public ActionResult Index()
         {
-            return View(db.SanPhams.Where(n=>n.DaXoa==false));
+            return View(db.SanPhams.Where(n=>n.DaXoa==false).OrderByDescending(n=>n.MaSP));
         }
 
         [HttpGet]
@@ -24,10 +25,35 @@ namespace WebsiteBanHang.Controllers
             return View();
         }
 
+        [ValidateInput(false)]
         [HttpPost]
-        public ActionResult TaoMoi(HttpPostedFileBase HinhAnh)
+        public ActionResult TaoMoi(SanPham sp, HttpPostedFileBase HinhAnh)
         {
-            return View();
+            //Xử lý Dropdownlist
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai");
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX");
+
+            //Xử ly hình ảnh
+            if (HinhAnh.ContentLength > 0)
+            {
+                var FileName_    = Path.GetFileName(HinhAnh.FileName);
+                var Path_ = Path.Combine(Server.MapPath("~/Content/HinhAnhSP"), FileName_);
+                if (System.IO.File.Exists(Path_))
+                {
+                    ViewBag.Upload = "Hình ảnh đã tồn tại";
+                }
+                else
+                {
+                    HinhAnh.SaveAs(Path_);
+                    //Session["ssTenHinh"] = HinhAnh.FileName;
+                    //ViewBag.ssTenHinh= "";
+                }
+                sp.HinhAnh = FileName_;
+            }
+            db.SanPhams.Add(sp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
