aad3e6b32b6fa8e240b83a28dcf6f5733c850402
QuangMinh <nguyenquangminh2912@gmail.com>
2017-09-01 16:14:36 +0700

    01/09 Tao moi san pham (tt)

--- WebsiteBanHang/Controllers/QuanLySanPhamController.cs
+++ WebsiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -27,7 +27,7 @@ namespace WebsiteBanHang.Controllers
 
         [ValidateInput(false)]
         [HttpPost]
-        public ActionResult TaoMoi(SanPham sp, HttpPostedFileBase HinhAnh)
+        public ActionResult TaoMoi(SanPham sp, HttpPostedFileBase[] HinhAnh)
         {
             //Xử lý Dropdownlist
             ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
@@ -35,9 +35,9 @@ namespace WebsiteBanHang.Controllers
             ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX");
 
             //Xử ly hình ảnh
-            if (HinhAnh.ContentLength > 0)
+            if (HinhAnh[0].ContentLength > 0)
             {
-                var FileName_    = Path.GetFileName(HinhAnh.FileName);
+                var FileName_    = Path.GetFileName(HinhAnh[0].FileName);
                 var Path_ = Path.Combine(Server.MapPath("~/Content/HinhAnhSP"), FileName_);
                 if (System.IO.File.Exists(Path_))
                 {
@@ -45,7 +45,7 @@ namespace WebsiteBanHang.Controllers
                 }
                 else
                 {
-                    HinhAnh.SaveAs(Path_);
+                    HinhAnh[0].SaveAs(Path_);
                     //Session["ssTenHinh"] = HinhAnh.FileName;
                     //ViewBag.ssTenHinh= "";
                 }
@@ -54,6 +54,30 @@ namespace WebsiteBanHang.Controllers
             db.SanPhams.Add(sp);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult ChinhSua(int? id)
+        {
+            //Lấy sản phẩm từ Model để chỉnh sửa từ id
+            if (id == null)
+            {
+                Response.StatusCode= 404;
+                return null;
+            }
+            
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id);
+            if (sp == null)
+            {
+                return HttpNotFound();       
+            }
+
+            //Xử lý Dropdownlist
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC",sp.MaNCC);
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai", sp.MaLoaiSP);
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX", sp.MaNSX);
+
+            return View(sp);
         }
     }
 }
